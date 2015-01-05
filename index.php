bc32ac072251fee4c63054b9d3d6a3202171d96d
root <root@ip-172-31-31-6.us-west-1.compute.internal>
2015-01-04 23:08:08 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,418 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Kurt's home page</title>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Permanent+Marker">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Walter+Turncoat">
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+	<link href="navigation.css" rel="stylesheet" type="text/css" />
+	<link href="index.css" rel="stylesheet" type="text/css" />
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	<script type="text/javascript"></script>
+	<script src="index.js"></script>
+
+<!--Query the database for the selected beer type-->
+
+</head>
+
+<body>
+	<?php include 'navigation.php';?>
+
+	<!-- Introduction -->
+	<div class="banner">
+		<img src="/resources/P1110300-02-04.jpg">
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xs-9">
+				<div class="entry">
+					<div class="entry-content">
+						<br>
+						<br>
+						<p>Hi, welcome to my personal website!  This page is currently intended to serve several purposes: it is part demo page
+							(showing responsive web design), part portal (check out the as-yet-unfinished
+							<a href="bottles.php">beer wall</a>, and partly autobiographical. Enjoy!</p>
+					</div>
+				</div>
+				<div class="entry">
+					<div class="entry-header">
+						<h2>Work</h2>
+					</div>
+					<div class="entry-content">
+						<p>I teach Chemistry, Physics, AP Chemistry, and AP Environmental Science at 
+							<a href="http://www.villagechristian.org/">Village Christian School</a> in Sun Valley, CA. I maintain a mostly informational website for my students: 
+							<a href="https://kleinsclasses.wikispaces.com/">Mr. Klein's Classes</a>. I also advise the Engineering Team, which finished 8th overall
+							at the JPL Invention Challenge on December 5th.  
+							You can read about it <a href="http://www.jpl.nasa.gov/events/inventionchallenge/2014/index.cfm">here</a> (pictures to follow shortly, I expect)
+							 or <a href="http://www.villagechristian.org/news-items/engineering-club-competes-at-jpl.aspx">here</a>.</p>
+							<p>My current goal is to find employment as a web or 
+							software developer. The reasons are numerous, but at or near the top of the list is my desire to think and problem-solve full-time.</p>
+					</div>
+				</div>
+				<div class="entry">
+					<div class="entry-header">
+						<h2>Play</h2>
+					</div>
+					<div class="entry-content">
+						<div class="trips">
+
+							<div class="trip active-photo">
+								<div class="description row">
+									<div class="col-sm-3">
+										<p class="date">
+											Aug-Sept 2014
+										</p>
+									</div>
+									<div class="col-sm-6">
+										<p class="location">
+											Bishop Pass, Sierra Nevada, CA
+										</p>
+									</div>
+									<div class="col-sm-3">
+										<p class="days">
+											3 days
+										</p>
+									</div>
+								</div>
+								<div class="photo row" style="">
+									<div class="col-sm-3 arrow-prev">
+										<p><</p>
+									</div>
+									<div class="col-sm-6">
+										<div class="slide active-slide">
+											<img src="/resources/slides/Bishop_Pass-8-2014/P1110181.jpg">
+											<p class="caption">Second backpacking trip of the summer, covering fewer miles than on the first trip so that there was more time to play and explore around the lakes</p>
+										</div>
+										<div class="slide">
+											<img src="/resources/slides/Bishop_Pass-8-2014/P1110200.jpg">
+											<p class="caption">Golden-mantled ground squirrel, my dad tells me</p>
+										</div>
+										<div class="slide">
+											<img src="/resources/slides/Bishop_Pass-8-2014/P1110236.jpg">	
+											<p class="caption">Flowers hanging to rock wall near creek</p>								
+										</div>
+										<div class="slide">
+											<img src="/resources/slides/Bishop_Pass-8-2014/P1110255.jpg">
+											<p class="caption">Mt. Agassiz</p>					
+										</div>
+										<div class="slide">
+											<img src="/resources/slides/Bishop_Pass-8-2014/P1110258.jpg">
+											<p class="caption">Playing battleship in the tent</p>
+										</div>
+										<div class="slide">
+											<img src="/resources/slides/Bishop_Pass-8-2014/P1110268.jpg">
+											<p class="caption">Sunrise</p>
+										</div>
+									</div>
+									<div class="col-sm-3 arrow-next">
+										<p>></p>
+									</div>
+								</div>
+							</div>
+
+							<div class="trip">
+								<div class="description row">
+									<div class="col-sm-3">
+										<p class="date">
+											July-Aug 2014
+										</p>
+									</div>
+									<div class="col-sm-6">
+										<p class="location">
+											Big Bear, CA
+										</p>
+									</div>
+									<div class="col-sm-3">
+										<p class="days">
+											10 days
+										</p>
+									</div>
+								</div>
+								<div class="photo row" style="">
+									<div class="col-sm-3 arrow-prev">
+										<p><</p>
+									</div>
+									<div class="col-sm-6">
+										<div class="slide active-slide">
+											<img src="/resources/slides/Big_Bear-7-2014/P1100822.jpg">
+											<p class="caption">A stormy sunset over the lake</p>
+										</div>
+										<div class="slide">
+											<img src="/resources/slides/Big_Bear-7-2014/P1100829.jpg">
+											<p class="caption">I'm a cloud fan</p>
+										</div>
+										<div class="slide">
+											<img src="/resources/slides/Big_Bear-7-2014/P1100888.jpg">
+											<p class="caption">Dirty lupine leaves from raindrop-induced mud-splatter</p>									
+										</div>
+										<div class="slide">
+											<img src="/resources/slides/Big_Bear-7-2014/P1100940.jpg">
+											<p class="caption">Flowers and rusty mining equipment in Holcomb Valley</p>									
+										</div>
+										<div class="slide">
+											<img src="/resources/slides/Big_Bear-7-2014/P1100810.jpg">
+											<p class="caption">This was practically a family reunion, with 10 of us in the cabin for several days</p>								
+										</div>
+									</div>
+									<div class="col-sm-3 arrow-next">
+										<p>></p>
+									</div>
+								</div>
+							</div>
+
+							<div class="trip">
+								<div class="description row">
+									<div class="col-sm-3">
+										<p class="date">
+											July 2014
+										</p>
+									</div>
+									<div class="col-sm-6">
+										<p class="location">
+											Cottonwood Lakes Basin, Sierra Nevada, CA
+										</p>
+									</div>
+									<div class="col-sm-3">
+										<p class="days">
+											2 days
+										</p>
+									</div>
+								</div>
+								<div class="photo row" style="">
+									<div class="col-sm-3 arrow-prev">
+										<p><</p>
+									</div>
+									<div class="col-sm-6">
+										<div class="slide active-slide">
+											<img src="/resources/slides/Cottonwood_Lakes-7-2014/P1100586.jpg">
+											<p class="caption">My son's first backpacking trip! Storm clearing our first day</p>
+										</div>
+										<div class="slide">
+											<img src="/resources/slides/Cottonwood_Lakes-7-2014/P1100612.jpg">
+											<p class="caption">He loves the moon, so I took a 6 a.m. picture</p>								
+										</div>
+										<div class="slide">
+											<img src="/resources/slides/Cottonwood_Lakes-7-2014/P1100672.jpg">
+											<p class="caption">Sunrise in the Cottonwood Lakes Basin</p>
+										</div>
+										<div class="slide">
+											<img src="/resources/slides/Cottonwood_Lakes-7-2014/P1100691.jpg">
+											<p class="caption">We were only out one night but hiked 14 miles, including an ascent of Army Pass</p>
+										</div>
+									</div>
+									<div class="col-sm-3 arrow-next">
+										<p>></p>
+									</div>
+								</div>
+							</div>
+
+							<div class="trip">
+								<div class="description row">
+									<div class="col-sm-3">
+										<p class="date">
+											May 2014
+										</p>
+									</div>
+									<div class="col-sm-6">
+										<p class="location">
+											Big Bear, CA
+										</p>
+									</div>
+									<div class="col-sm-3">
+										<p class="days">
+											3 days
+										</p>
+									</div>
+								</div>
+								<div class="photo row" style="">
+									<div class="col-sm-3 arrow-prev">
+										<p><</p>
+									</div>
+									<div class="col-sm-6">
+										<div class="slide active-slide">
+											<img src="/resources/slides/Big_Bear-5-2014/P1100212.jpg" title="lupine flowers">
+											<p class="caption">I don't take as many pictures in Big Bear as on other trips, perhaps because I go there a few times a year, but I will not usually pass up the opportunity to photograph lupines!</p>
+										</div>
+										<div class="slide">
+											<img src="/resources/slides/Big_Bear-5-2014/P1100215.jpg" title="lupine leaves">
+											<p class="caption">I like the leaf shape</p>
+										</div>
+									</div>
+									<div class="col-sm-3 arrow-next">
+										<p>></p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="entry">
+					<div class="entry-header">
+						<h2>Projects</h2>
+					</div>
+					<div class="entry-content">
+						<br>
+						<ul>Website (general)
+							<li>install the Laravel PHP framework</li>
+							<li>add footers</li>
+							<li>create config files</li>
+						</ul>
+						<ul>Beer Wall
+							<li>Code</li>
+								<ul>
+									<li>filter dropdown menus as well as bottle images</li>
+									<li>install Laravel</li>
+									<li>add more dropdown options</li>
+									<li>add custom searches</li>
+									<li>make dynamically-generated pages for each bottle selection</li>
+								</ul>
+							<li>Database</li>
+								<ul>
+									<li>add bottles</li>
+									<li>add outside links</li>
+								</ul>
+							<li>Photos</li>
+								<ul>
+									<li>adjust white balance</li>
+									<li>crop the background out of the bottles</li>
+									<li>make PNGs</li>
+									<li>normalize sizes as much as possible</li>
+								</ul>
+						</ul>
+						<ul>Other projects
+							<li>add pages for other projects (like the solar system animation)</li>
+						</ul>
+
+					</div>
+				</div>
+			</div>
+
+			<div class="col-xs-3 sidebar">
+				<p class="caption">Above: Saddlerock Lake, Bishop Pass area, Sierra Nevada, CA, 2014-09-01; Panasonic Lumix LX5</p>
+<!-- LASTFM -->	 
+	            <div class="container-fluid sidebar-header">
+					<div class="row">
+						<div class="col-sm-2 icons">
+							<a href="http://www.last.fm/user/Kurt_K">
+								<img src="/resources/icons/lastfm.png">
+							</a>
+						</div>
+
+						<div class="col-sm-10 description">
+							<p>Last.fm</p>
+						</div>
+					</div>
+				</div>	            
+		        <hr>
+		        <p>None of the Last.fm widgets I have found do exactly what I want, so I plan to make my own soon.</p>
+	            <br>
+<!-- GITHUB -->	            
+	            <div class="container-fluid sidebar-header">
+					<div class="row">
+						<div class="col-sm-2 icons">
+							<a href="https://github.com/fujifilmfan">
+								<img src="/resources/icons/github.png">
+							</a>
+						</div>
+
+						<div class="col-sm-10 description">
+							<p>GitHub</p>
+						</div>
+					</div>
+				</div>	            
+		        <hr>
+		        <p>Fork me.</p>
+	            <br>
+<!-- TWITTER -->				
+				<div class="container-fluid sidebar-header">
+					<div class="row">
+						<div class="col-sm-2 icons">
+							<a href="https://twitter.com/kurtklein">
+								<img src="/resources/icons/twitter.png">
+							</a>
+						</div>
+
+						<div class="col-sm-10 description">
+							<p>Twitter</p>
+						</div>
+					</div>
+				</div>
+				<hr>
+				<a class="twitter-timeline"
+					href="https://twitter.com/kurtklein"
+					data-widget-id="536058606106714112"
+					data-chrome="noborders nofooter">Tweets by @kurtklein
+				</a>
+	            <script>!function(d,s,id)
+	            	{
+	            	var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+	            		if(!d.getElementById(id))
+	            			{
+	            			js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);
+	            			}
+	            	}(document,"script","twitter-wjs");
+
+	            </script>
+	            <br>
+	            <br>
+<!-- DIIGO -->
+	            <div class="container-fluid sidebar-header">
+					<div class="row">
+						<div class="col-sm-2 icons">
+							<a href="https://www.diigo.com/user/acetone">
+								<img src="/resources/icons/diigo.png">
+							</a>
+						</div>
+
+						<div class="col-sm-10 description">
+							<p>Diigo</p>
+						</div>
+					</div>
+				</div>
+				<hr>
+		        <script type="text/javascript" src="https://www.diigo.com/roll2/linkrolls?v=4&username=acetone&icon=false&l_type=0&count=5&desc=1&style=standard&title=5%20Most%20Recent%20Bookmarks&tags=" ></script>
+	            <br>
+<!-- LINKEDIN -->
+	            <div class="container-fluid sidebar-header">
+					<div class="row">
+						<div class="col-sm-2 icons">
+							<a href="https://www.linkedin.com/pub/kurt-klein/">
+								<img src="/resources/icons/linkedin.png">
+							</a>
+						</div>
+
+						<div class="col-sm-10 description">
+							<p>LinkedIn</p>
+						</div>
+					</div>
+				</div>
+				<hr>
+	            <br>
+<!-- FACEBOOK -->
+	            <div class="container-fluid sidebar-header">
+					<div class="row">
+						<div class="col-sm-2 icons">
+							<a href="https://www.diigo.com/user/acetone">
+								<img src="/resources/icons/facebook.png">
+							</a>
+						</div>
+
+						<div class="col-sm-10 description">
+							<p>Facebook</p>
+						</div>
+					</div>
+				</div>	            
+		        <hr>
+		        <p>I mainly use Facebook to update my privacy settings for Facebook.</p>
+				<!-- <a href="http://lastfm.terpilovsky.ru/artists/">
+					<img src="http://lastfm.terpilovsky.ru/artists/files/?username=Kurt_K&type=list&period=3month&limit=5&size=large" alt="Top Artists" /></a>
+				<a href="http://lastfm.terpilovsky.ru/artists/">Last.fm Top Artists Generator</a> -->
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
