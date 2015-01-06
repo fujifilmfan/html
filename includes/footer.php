9678bbe9bee2aca75c93ff6bfa20475c191f268a
fujifilmfan <fuji.film.fan@gmail.com>
2015-01-05 20:11:30 -0800

    added new directory for includes

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<?php
+
+	// jQuery from https://developers.google.com/speed/libraries/devguide
+	// minified Twitter Bootstrap from http://www.bootstrapcdn.com/
+
+	echo
+	    '<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script> 
+	    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>';
+
+?>
---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<?php
+	
+	// minified Twitter Bootstrap from http://www.bootstrapcdn.com/
+	
+	echo
+		'<meta charset="utf-8" />
+		<link href="/includes/navigation.css" rel="stylesheet" type="text/css" />
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css">';
+
+?>
