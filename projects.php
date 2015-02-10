e0e11193f3b8ebc07df4c8fca7d1976a8cf8fab2
fujifilmfan <fuji.film.fan@gmail.com>
2015-02-09 21:13:01 -0800

    added projects page and first supporting page

---
+++ projects.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+	<?php include "includes/header.php";?>
+	<title>Kurt Klein | Beer Wall | Coding Projects</title>
+	<meta name="description" content="Kurt Klein's home page, contact information, virtual Beer Wall, and other coding projects.">
+	<meta name="keywords" content="Kurt Klein, Kurt, Klein, beer, bottles, beer bottles, backpacking, pictures, photography, web apps, projects">
+	<link href="projects.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+	<?php include "includes/navigation.php";?>
+
+	<!-- Introduction -->
+	<div class="banner">
+		<img src="resources/P1110300-02-04.jpg">
+	</div>
+	<br />
+	<h2>Projects</h2>
+	<br />
+	<br />
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xs-2">
+			</div>
+			<div class="col-xs-8">
+				<div class="row">
+					<div class="col-sm-5">
+						<div class="preview">
+							<a href="Codecademy-news_reader.php"><img src="resources/slides/projects/Codecademy-news_reader.png"></a>
+							<p class="caption">Project: News Reader (Codecademy)</p>
+							<p class="caption">Emphasis: jQuery events</p>
+							<p class="caption">Completed: 26 October 2014</p>
+							<p class="caption">Modifications: arrow key navigation, looping through articles, bg image fills space w/o tiling</p>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<p></p>
+					</div>
+					<div class="col-sm-5">
+						<div class="preview">
+							
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-2">
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+	<?php include "includes/footer.php";?>
+	<script src="index.js"></script>
+</body>
+</html>
