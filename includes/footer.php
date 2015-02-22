6625f66b93123378e654e76ceef63953e40fc0f4
fujifilmfan <fuji.film.fan@gmail.com>
2015-02-21 20:56:42 -0800

    added copyright statement to footer

--- includes/footer.php
+++ includes/footer.php
@@ -6,6 +6,6 @@
 	echo
 	    '<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script> 
 	    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>';
-
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+		<div id="copyright">© Copyright 2015 Kurt Klein</div>';
 ?>
