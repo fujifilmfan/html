faa2cbb631a5615121feeadcb299ffc0a35318c8
fujifilmfan <fuji.film.fan@gmail.com>
2015-02-23 22:03:17 -0800

    fixed link error

--- includes/navigation.php
+++ includes/navigation.php
@@ -12,17 +12,17 @@
 			        <span class="icon-bar"></span>
 			        <span class="icon-bar"></span>
 			      </button>
-			      <a class="navbar-brand" href="/kurtaklein/index.php">Kurt Klein</a>
+			      <a class="navbar-brand" href="/index.php">Kurt Klein</a>
 			    </div>
 
 			    <!-- Collect the nav links, forms, and other content for toggling -->
 			    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			      <ul class="nav navbar-nav">
-			        <li><a href="/kurtaklein/index.php">Home</a></li>
+			        <li><a href="/index.php">Home</a></li>
 			      </ul>
 			      <ul class="nav navbar-nav navbar-right">
-			        <li><a href="/kurtaklein/bottles.php">Beer Wall</a></li>
-			        <li><a href="/kurtaklein/projects.php">Projects</a></li>
+			        <li><a href="/bottles.php">Beer Wall</a></li>
+			        <li><a href="/projects.php">Projects</a></li>
 			      </ul>
 			    </div><!-- /.navbar-collapse -->
 			  </div><!-- /.container-fluid -->
